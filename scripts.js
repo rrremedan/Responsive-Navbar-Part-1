1db4faa9881b0d21bc95b6f3bcd0460c347af495
rrremedan <semar893@hsd.k12.or.us>
2022-05-18 15:23:34 -0700

    add Js

--- scripts.js
+++ scripts.js
@@ -1,4 +1,26 @@
+var mobileBreakpoint =600; 
 /*when the page loads, add an open class to nav if
-    the screen is wide enough. 
+    the screen is wide enough. */
 
-wu
+window.onload = function() {
+    var screenWidth = window.innerWidth;
+    var globalNavbar = document.getElementById ("global-nav");
+
+    // add 'open' class to the ul in the navbar if wider than  
+    // the breakpoint 
+    if (screenWidth > mobileBreakpoint) {
+        globalNavbar.classList.add("open");
+    }
+}
+window.onresize = function() {
+    var screenWidth = window.innerWidth;
+    var mobileBreakpoint =600; 
+    var globalNavbar = document.getElementById ("global-nav");
+
+    if (screenWidth > mobileBreakpoint) {
+        globalNavbar.classList.add("open");
+    }
+    else {
+        globalNavbar.classList.remove("open");
+    }
+}
