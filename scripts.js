05df4ac22bad6e8d1dd6a70b508d943a080ca44f
rrremedan <semar893@hsd.k12.or.us>
2022-05-20 17:11:59 -0700

    scripts.js

--- scripts.js
+++ scripts.js
@@ -1,26 +1,37 @@
 var mobileBreakpoint =600; 
-/*when the page loads, add an open class to nav if
-    the screen is wide enough. */
 
+/*when the page loads, add an open class to nav if the screen is wide enough. */
 window.onload = function() {
     var screenWidth = window.innerWidth;
     var globalNavbar = document.getElementById ("global-nav");
+    var toggle = document.getElementById("menu-toggle");
 
     // add 'open' class to the ul in the navbar if wider than  
     // the breakpoint 
     if (screenWidth > mobileBreakpoint) {
         globalNavbar.classList.add("open");
+        toggle .classList.add("open");
+        // add  a toggle click function, which serves as an on-off button  
+    }
+    toggle.onclick = function() {
+        if (globalNavbar.classList.contains("open")){
+            globalNavbar.classList.add("open");
+        }
+
+        
     }
 }
 window.onresize = function() {
     var screenWidth = window.innerWidth;
-    var mobileBreakpoint =600; 
-    var globalNavbar = document.getElementById ("global-nav");
+    var globalNavbar = document.getElementById("global-nav")
+    var toggle = document.getElementById("menu-toggle")
 
     if (screenWidth > mobileBreakpoint) {
         globalNavbar.classList.add("open");
+        toggle.classList. add("open");
     }
     else {
         globalNavbar.classList.remove("open");
+        toggle.classList.remove("open")
     }
 }
